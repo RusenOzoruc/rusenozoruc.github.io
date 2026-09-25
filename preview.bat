a7f734f61dc410ebd8696a59f9c9fe0925b8de90
RusenOzoruc <rusenmustafaozoruc@gmail.com>
2026-09-25 23:09:41 +0300

    Remove Windows script preview launcher

--- preview.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title RMO Website Preview
-cd /d "%~dp0"
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0preview-windows.ps1"
-if errorlevel 1 (
-  echo.
-  echo The preview could not start.
-  pause
-)
